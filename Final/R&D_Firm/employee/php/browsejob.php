1b7a08bbe6c4b788876dd765556a9b2d3787001e
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-03 14:49:17 +0600

    Add browse job navbar created

---
+++ Final/R&D_Firm/employee/php/browsejob.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Browse job</title>
+        <link rel="stylesheet" href="../css/browsejob.css">
+    </head>
+    <body>
+        <nav class = "navbar">
+            <h2>NilKham</h2>
+            <ul class = "container">
+                <li><a>Home</a></li>
+                <li><a>About Us</a></li>
+                <li><a>Our services</a></li>
+                <li><a>Browse a job</a></li>
+                <li><a>Copntacy Us</a></li>
+            </ul>
+
+        </nav>
+    </body>
+</html>
