a9f6ef9e92ca38fd1aa276dc670c4239c2bad4ae
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-03 15:47:42 +0600

    database connection added

---
+++ Final/R&D_Firm/employee/db/db.php
@@ -0,0 +1,16 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$dbname="r&d_firm";
+ 
+$conn = new mysqli($host,$user,$pass,$dbname);
+ 
+if($conn->connect_error)
+{
+    die("Connect lost". $conn->connect_error);        
+}
+ 
+ 
+ 
+?>
