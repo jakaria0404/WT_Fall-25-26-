21d9d3ee269336bd04b54b7ae5d11811460af8c0
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-04 11:27:53 +0600

    dashboard nav bar desined

--- Final/R&D_Firm/employee/php/dashboard.php
+++ Final/R&D_Firm/employee/php/dashboard.php
@@ -6,7 +6,21 @@ if(!isset($_SESSION["username"])){
 ?>
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>Browse job</title>
+        <link rel="stylesheet" href="../css/dashboard.css">
+    </head>
     <body>
+         <nav class = "navbar">
+            <h2>NilKham</h2>
+            <ul class = "container">
+                <li><a>Home</a></li>
+                <li><a>About Us</a></li>
+                <li><a>Our services</a></li>
+                <li><a>Browse a job</a></li>
+                <li><a>Copntacy Us</a></li>
+            </ul>
+        </nav>
         <h2>hello</h2>
         <a href = "logout.php" class="logout_btn">Logout</a>
     </body>
