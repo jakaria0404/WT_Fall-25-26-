4920a7a169e1b373e79e606175fc31857935e827
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-04 20:12:43 +0600

    dashboard css updated

--- Final/R&D_Firm/employee/php/dashboard.php
+++ Final/R&D_Firm/employee/php/dashboard.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
+include "../db/db.php";
 if(!isset($_SESSION["username"])){
-    header("Location: login.php");
+    header("Location: login.php");   
 }
+$sql = "SELECT title ,description ,requirement, type FROM create_job";
+$result = mysqli_query($conn,$sql);
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,6 +24,30 @@ if(!isset($_SESSION["username"])){
                 <li><a>Copntacy Us</a></li>
             </ul>
         </nav>
+        <div class = "job-section">
+            <h1 class = "job-title"></h1>
+            <div class = "job_grid">
+                <?php
+                if(mysqli_num_rows($result)>0){
+                    while($row = mysqli_fetch_assoc($result))
+                    {
+                ?>
+                <div class = "job_part">
+                    <span class = "view_job"><?php echo $row['type']; ?></span>
+                    <h3><?php echo $row['title']; ?></h3>
+                    <p>Description: <?php echo $row['description']; ?></p>
+                    <p>Requirement: <?php echo $row['requirement']; ?></p>
+                </div>
+                <?php 
+                    }
+                }
+                else {
+                    echo "NO job found";
+                }
+                ?>
+                
+            </div>
+        </div>
         <h2>hello</h2>
         <a href = "logout.php" class="logout_btn">Logout</a>
     </body>
