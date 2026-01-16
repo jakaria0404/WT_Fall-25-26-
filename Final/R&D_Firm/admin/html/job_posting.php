41c16535f74d53f3cf4a23834b0222076dfe7bd0
jakaria0404 <jakariashoriful@gmail.com>
2026-01-16 17:36:58 +0600

    Job posting page implemented

---
+++ Final/R&D_Firm/admin/html/job_posting.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Posting - Admin</title>
+    <link rel="stylesheet" href="../css/dashboard_page/header.css">
+    <link rel="stylesheet" href="../css/dashboard_page/sidebar.css">
+    <link rel="stylesheet" href="../css/job_posting_page/job_posting.css">
+
+    </head>
+<body>
+    <div class="dashboard-container">
+        <?php include "sidebar.php"; ?>
+        <main class="main-content">
+            <?php include "header.php"; ?>
+            <div class="page-content">
+            <h2>Post a Job</h2>
+
+</body>
+</html>
