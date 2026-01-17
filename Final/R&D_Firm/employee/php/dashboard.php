526e539f949d3100677dc1576dbdf7f6360ec548
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-18 04:27:45 +0600

    dashboard task count add

--- Final/R&D_Firm/employee/php/dashboard.php
+++ Final/R&D_Firm/employee/php/dashboard.php
@@ -5,14 +5,21 @@ if(!isset($_SESSION["username"])){
     header("Location: login.php");   
     exit();
 }
-$sql = "SELECT id, title ,description ,requirement, type FROM create_job";
-$result = mysqli_query($conn,$sql);
+$currentUser = $_SESSION['username'];
+$userQuery = "SELECT unique_id FROM users WHERE username = '$currentUser'";
+$userResult = mysqli_query($conn, $userQuery);
+$userData = mysqli_fetch_assoc($userResult);
+$userId = $userData['unique_id'];
+
+$sql_assigned = "SELECT id FROM tasks WHERE employee_id = '$userId'";
+$result_assigned = mysqli_query($conn, $sql_assigned);
+$assignedCount = mysqli_num_rows($result_assigned);
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Browse job</title>
-        <link rel="stylesheet" href="../css/dashboard.css">
+        <link rel="stylesheet" href="../css/dash.css">
     </head>
     <body>
          <nav class = "navbar">
@@ -22,9 +29,15 @@ $result = mysqli_query($conn,$sql);
                 <li><a>About Us</a></li>
                 <li><a>Our services</a></li>
                 <li><a href = "browsejob.php">Browse job</a></li>
+                <li><a href = "profile.php">Profile</a></li>
                 <li><a>Contact Us</a></li>
             </ul>
         </nav>
+            <div class="sidebar">
+            <a href="dashboard.php"style="background-color: #388e3c;">Dashboard</a>
+            <a href="profile.php">Profile</a>
+            <a href="myjob.php">My Job Application</a>
+        </div>
         <a href = "logout.php" class="logout_btn">Logout</a>
     </body>
 </html>
