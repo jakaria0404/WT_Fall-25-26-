b376fb61ba7992e4d7188e8c6736ca24b07bc900
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-18 18:30:04 +0600

    created user profile

---
+++ Final/R&D_Firm/employee/php/userprofile.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profile</title>
+    <link rel="stylesheet" href="../css/profile.css">
+</head>
+<body>
+    <nav class="navbar">
+        <h2>NilKham</h2>
+        <ul class="container">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="browsejob.php">Browse job</a></li>
+            <li><a href="profile.php">Profile</a></li>
+
+        </ul>
+    </nav>
+
+    <div class="sidebar">
+        <a href="dashboard.php">Dashboard</a>
+        <a href="profile.php" style="background-color: #388e3c;">Profile</a>
+        <a href="myjob.php">My Job Application</a>
+        <a href="task.php">Tasks</a>
+    </div>
+</body>
+</html>
