5eef1437cb7d6e419618e86ae8e3cc6bac046a85
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-18 18:52:02 +0600

    add html

---
+++ Final/R&D_Firm/employee/php/userjob.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Job Applications</title>
+    <link rel="stylesheet" href="../css/myjob.css">
+</head>
+<body>
+    <nav class="navbar">
+        <h2>NilKham</h2>
+        <ul class="container">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="browsejob.php">Browse job</a></li>
+            <li><a href="profile.php">Profile</a></li>
+        </ul>
+    </nav>
+
+    <div class="sidebar">
+        <a href="profile.php">Profile</a>
+        <a href="myjob.php" style="background-color: #45a049;">My Job Application</a>
+    </div>
+
+    <div class="content">
+        <div class="table">
+            <h1>My Job Applications</h1>
+            <table class="job-table">
+                <tr>
+                    <th><b>Job Title</b></th>
+                    <th><b>Type</b></th>
+                    <th><b>Status</b></th>
+                    <th><b>Phase</b></th>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
