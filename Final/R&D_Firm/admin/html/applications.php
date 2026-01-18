15fc4f2cdc61cc6c677872c9afa575961608f3c1
jakaria0404 <jakariashoriful@gmail.com>
2026-01-19 03:16:39 +0600

    admin applications viewing page planted now

---
+++ Final/R&D_Firm/admin/html/applications.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Applications - Admin</title>
+
+     <link rel="stylesheet" href="../css/dashboard_page/dashboard.css">
+    <link rel="stylesheet" href="../css/dashboard_page/header.css">
+    <link rel="stylesheet" href="../css/applications_page/applications.css">
+    <link rel="stylesheet" href="../css/dashboard_page/sidebar.css">
+</head>
+    <body>
+        <div class="dashboard-container">
+        <?php include "sidebar.php"; ?>
+        <main class="main-content">
+            <?php include "header.php"; ?>
+                
+            <div class="container">
+                <h2>Job Applications</h2>
