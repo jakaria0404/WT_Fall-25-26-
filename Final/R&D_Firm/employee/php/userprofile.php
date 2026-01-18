d19bee0e7de0ac87dee53dc942a794653d8f3f46
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-18 18:33:31 +0600

    add profile form

--- Final/R&D_Firm/employee/php/userprofile.php
+++ Final/R&D_Firm/employee/php/userprofile.php
@@ -16,10 +16,37 @@
     </nav>
 
     <div class="sidebar">
-        <a href="dashboard.php">Dashboard</a>
         <a href="profile.php" style="background-color: #388e3c;">Profile</a>
         <a href="myjob.php">My Job Application</a>
-        <a href="task.php">Tasks</a>
+    </div>
+        <h2 class ="profile">Welcome <?php echo $_SESSION['username'];?></h2>
+
+    <div class="profile">
+        <h2>Update Profile</h2>
+        
+        <?php if($msg != ""): ?>
+            <p style="color: green; text-align:center; font-weight: bold;"><?php echo $msg; ?></p>
+        <?php endif; ?>
+
+        <form method="POST">
+            <div class="group">
+                <label>First Name</label><br>
+                <input type="text" name="first_name" value="<?php echo $user['first_name'] ?? ''?>"> 
+            </div>
+            <div class="group">
+                <label>Last Name</label><br>
+                <input type="text" name="last_name" value="<?php echo $user['last_name'] ?? ''?>"> 
+            </div>
+            <div class="group">
+                <label>Username</label><br>
+                <input type="text" name="username" value="<?php echo $user['username'] ?? ''?>"> 
+            </div>
+            <div class="group">
+                <label>Email</label><br>
+                <input type="email" name="email" value="<?php echo $user['email'] ?? ''?>"> <br><br>
+            </div>
+            <button type="submit" class="btn">Save Changes</button>
+        </form>
     </div>
 </body>
 </html>
