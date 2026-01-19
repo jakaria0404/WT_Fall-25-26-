1bac2276d9e32f4054320639de1deff0243d2a52
jakaria0404 <jakariashoriful@gmail.com>
2026-01-19 19:45:53 +0600

    contact page landed now

---
+++ Final/R&D_Firm/admin/html/contact_messages.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Messages</title>
+    <link rel="stylesheet" href="../css/dashboard_page/header.css">
+    <link rel="stylesheet" href="../css/dashboard_page/sidebar.css">
+    <link rel="stylesheet" href="../css/dashboard_page/dashboard.css">
+    <link rel="stylesheet" href="../css/contact_page/contact_messages.css">
+</head>
+<body>
+<div class="dashboard-container">
+    <?php include "sidebar.php"; ?>
+    <main class="main-content">
+        <?php include "header.php"; ?>
+        <div class="page-content">
+            <h3>Contact Messages</h3>
