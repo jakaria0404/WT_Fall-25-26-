17d36124158ec93533392366295b2613ab35d2ed
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-19 20:30:20 +0600

    browsejob footer add

--- Final/R&D_Firm/employee/php/browsejob.php
+++ Final/R&D_Firm/employee/php/browsejob.php
@@ -42,5 +42,22 @@ $result = mysqli_query($conn,$sql);
                 }
                 ?>
         <a href = "logout.php" class="logout_btn">Logout</a>
+        <footer class="footer">
+            <div class="footer-container">
+                <div class="footer-section">
+                    <h3>Location</h3>
+                    <p>123 Innovation Street<br>Tech City, Banani<br>Bangladesh</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Follow Us</h3>
+                    <div class="social-links">
+                        <a class="social-icon">Instagram</a>
+                        <a class="social-icon">Facebook</a>
+                        <a class="social-icon">Twitter</a>
+                        <a class="social-icon">LinkedIn</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
