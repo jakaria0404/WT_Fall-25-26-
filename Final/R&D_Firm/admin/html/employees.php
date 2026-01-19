09c6667b9a8ac2d76ce2a2dc504721e48024e32a
jakaria0404 <jakariashoriful@gmail.com>
2026-01-19 14:35:44 +0600

    Employee page area spoted now

---
+++ Final/R&D_Firm/admin/html/employees.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Employees Admin</title>
+    <link rel="stylesheet" href="../css/dashboard_page/dashboard.css">
+    <link rel="stylesheet" href="../css/employee_page/employees.css">
+    <link rel="stylesheet" href="../css/dashboard_page/header.css">
+    <link rel="stylesheet" href="../css/dashboard_page/sidebar.css">
+</head>
+<body>
+
+<div class="dashboard-container">
+    <?php include "sidebar.php"; ?>
+
+    <main class="main-content">
+        <?php include "header.php"; ?>
+
+        <div class="page-content">
+            <h2>Employee Management</h2>
