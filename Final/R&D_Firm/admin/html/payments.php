e31cb0dd759707c6bc34d970696e754de33a283d
jakaria0404 <jakariashoriful@gmail.com>
2026-01-19 18:46:47 +0600

    Payment page landed now

---
+++ Final/R&D_Firm/admin/html/payments.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Payment Page</title>
+    <link rel="stylesheet" href="../css/dashboard_page/header.css">
+    <link rel="stylesheet" href="../css/dashboard_page/sidebar.css">
+    <link rel="stylesheet" href="../css/payments_page/payments.css">
+    <link rel="stylesheet" href="../css/dashboard_page/dashboard.css">
+</head>
+<body>
+<div class="dashboard-container">
+        <?php include "sidebar.php"; ?>
+        <main class ="main-content">
+            <?php include "header.php"; ?>
+            <div class="page-content">
+    <div class="my-form-box">
+        <h3>Make a Payment</h3>
+
+        
