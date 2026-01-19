4d6dbac13103f001f29a4b44720c28b4e68f641b
kamrul-hasan960 <hasankamrul06723@gmail.com>
2026-01-19 18:30:21 +0600

    contactus nav bar

---
+++ Final/R&D_Firm/employee/php/contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="css/contact.css">
+</head>
+<body>
+        <nav class = "navbar">
+            <h2>NilKham</h2>
+            <ul class = "container">
+                <li><a href = "home.php">Home</a></li>
+                <li><a>About Us</a></li>
+                <li><a>Our services</a></li>
+                <li><a href = "browsejob.php">Browse job</a></li>
+                <li><a href = "Contact.php">Contact Us</a></li>
+            </ul>
+        </nav>
+
+</body>
+</html>
